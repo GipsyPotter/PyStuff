027fae4e918863e4d16e5081a36907d03f65640c
ThePotter <nguyentadac@gmail.com>
2022-12-02 19:46:06 +0700

    update

--- 1.py
+++ 1.py
@@ -1,80 +1,95 @@
+import math
+
+
 class Point:
-	def __init__(self, x=0, y=0):
-		self.__x = x
-		self.__y = y
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
 
-	def get_x(self):
-		...
+    def get_x(self):
+        return self.__x
 
-	def get_y(self):
-		...
+    def get_y(self):
+        return self.__y
 
-	def set_x(self, x):
-		...
+    def set_x(self, x):
+        self.__x = x
 
-	def set_y(self, y):
-		...
+    def set_y(self, y):
+        self.__y = y
 
-	def distance(self, other_point) -> float:
-		...
+    def distance(self, other) -> float:
+        dis = math.sqrt((self.__x - other.__x) ** 2 + (self.__y - other.__y) ** 2)
+        return dis
 
-	# unit la don vi
-	def go_left(self, unit):
-		...
+    # unit la don vi
+    def go_left(self, unit):
+        self.__x -= unit
 
-	# lam tuong tu voi right up down
+    def go_right(self, unit):
+        self.__x += unit
+
+    def go_up(self, unit):
+        self.__y += unit
+
+    def go_down(self, unit):
+        self.__y -= unit
+
+
+# lam tuong tu voi right up down
 
 
 class Shape:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def area(self) -> float:
-		...
+    def area(self) -> float:
+        ...
 
-	def perimeter(self) -> float:
-		...
+    def perimeter(self) -> float:
+        ...
 
-	def export_point(self):
-		...
-	
-	# nhớ tìm hiểu thêm các phép biến hình như tịnh tiến, xoay, ...
-	# sau đó tự define hàm nhé!
+    def export_point(self):
+        ...
+
+
+# nhớ tìm hiểu thêm các phép biến hình như tịnh tiến, xoay, ...
+# sau đó tự define hàm nhé!
 
 
 class Triangle(Shape):
-	def __init__(self, a: Point, b: Point, c: Point):
-		# kiem tra hinh chu nhat co hop le hay chua
-		# neu chua thi phai in ra canh bao cho nguoi dung
-		# va yeu cau ho nhap lai
-		self.__a = a
-		self.__b = b
-		self.__c = c
+    def __init__(self, a: Point, b: Point, c: Point):
+        # kiem tra hinh chu nhat co hop le hay chua
+        # neu chua thi phai in ra canh bao cho nguoi dung
+        # va yeu cau ho nhap lai
+        self.__a = a
+        self.__b = b
+        self.__c = c
 
-	def area(self) -> float:
-		...
+    def area(self) -> float:
+        ...
 
-	def perimeter(self) -> float:
-		...
-	
+    def perimeter(self) -> float:
+        ...
+
 
 class Rectangle(Shape):
-	def __init__(self, a, b, c, d):
-		self.__a = a
-		self.__b = b
-		self.__c = c
-		self.__d = d
+    def __init__(self, a, b, c, d):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+        self.__d = d
 
-	# kiem tra hinh chu nhat co hop le hay chua
-	# neu chua thi phai in ra canh bao cho nguoi dung
-	# va yeu cau ho nhap lai
+    # kiem tra hinh chu nhat co hop le hay chua
+    # neu chua thi phai in ra canh bao cho nguoi dung
+    # va yeu cau ho nhap lai
 
-	def area(self) -> float:
-		...
+    def area(self) -> float:
+        ...
 
-	def perimeter(self) -> float:
-		...
+    def perimeter(self) -> float:
+        ...
 
 
 if __name__ == '__main__':
-	...
+    ...
